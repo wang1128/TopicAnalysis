801f3472acd463a35383d090d3a5252f0651ef79
wang1128 <wang1128@purdue.edu>
2016-05-19 17:21:00 +0800

    5.19

--- loadData.py
+++ loadData.py
@@ -1,5 +1,6 @@
 # author: Penghao Wang
 # -*- coding:utf-8 -*-
+#encoding=utf-8
 '''
  * ${导入数据 并清洗数据 from 36kr}
  #
@@ -16,25 +17,24 @@ def loadfiles():
     f = open('result_fetch.txt', 'r')
     articleList = []
     splitArtList = []  #每一篇文章分为 4个部分 index0 : url, index 1 : time, index 2: title, index 3 : content
+
     for line in f:
         articleList.append(line)
-
-
     for idx,article in enumerate(articleList):
         content = articleList[idx]
         splitContent = content.split('\t')
         splitArtList.append(splitContent)
 
+    return splitArtList
 
 
-    #a = articleList[0]
-    #b = a.split('\t')
-    #print b
-    print splitArtList[0]
-    print splitArtList[0][2]
 
 
-loadfiles()
+if __name__ == '__main__':
+    articleList = loadfiles()
+    print articleList[0][2]
+    seg_list = jieba.cut(articleList[0][2])
+    print "Default Mode:", " ".join(seg_list)
 
 
 

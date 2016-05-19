9e69144c4050fff6d77c42aa770ae4120c7871ea
wang1128 <wang1128@purdue.edu>
2016-05-19 15:12:01 +0800

    strart

--- loadData.py
+++ loadData.py
@@ -1,11 +1,41 @@
 # author: Penghao Wang
 # -*- coding:utf-8 -*-
 '''
- * ${导入数据 from 36kr}
+ * ${导入数据 并清洗数据 from 36kr}
  #
  * @version V1.0
  * @Author Penghao Wang
  * @Create ${2016.5.18} ${10:46}
  * @Copyright: Copyright 2016 zhizhu Technology
 '''
+
+import jieba
+import numpy as np
+
+def loadfiles():
+    f = open('result_fetch.txt', 'r')
+    articleList = []
+    splitArtList = []  #每一篇文章分为 4个部分 index0 : url, index 1 : time, index 2: title, index 3 : content
+    for line in f:
+        articleList.append(line)
+
+
+    for idx,article in enumerate(articleList):
+        content = articleList[idx]
+        splitContent = content.split('\t')
+        splitArtList.append(splitContent)
+
+
+
+    #a = articleList[0]
+    #b = a.split('\t')
+    #print b
+    print splitArtList[0]
+    print splitArtList[0][2]
+
+
+loadfiles()
+
+
+
 

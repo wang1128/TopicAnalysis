452aec5c03678d8da19512b11d2556ae04265d38
wang1128 <wang1128@purdue.edu>
2016-05-23 11:13:40 +0800

    5.23
    almost finished.

--- loadData.py
+++ loadData.py
@@ -9,7 +9,6 @@
  * @Create ${2016.5.18} ${10:46}
  * @Copyright: Copyright 2016 zhizhu Technology
 '''
-
 import jieba
 import numpy as np
 
@@ -27,14 +26,32 @@ def loadfiles():
 
     return splitArtList
 
-
-
+def calWordList(splitArtList):
+    wordList = []
+    for idx in range(len(splitArtList) - 1):
+        seg_list = list(jieba.cut(articleList[idx][3])) # idx 3 is the content
+        wordList = wordList + seg_list
+    return wordList
 
 if __name__ == '__main__':
     articleList = loadfiles()
-    print articleList[0][2]
-    seg_list = jieba.cut(articleList[0][2])
-    print "Default Mode:", " ".join(seg_list)
+    #print articleList[0][2]
+    #seg_list = list(jieba.cut(articleList[0][3])) #分词系统, 要统计词
+    #print seg_list
+    #print len(articleList)
+    seg_list = calWordList(articleList)  # total word in the word list
+    print len(seg_list)
+    setOfword = set(seg_list)
+    wList = list(setOfword)
+    for word in wList:
+        if seg_list.count(word)> 3000:
+            print word
+
+    #for item in seg_list:
+    #    if item != u'\uff1a':
+     #       print item
+
+    #print "Default Mode:", " ".join(seg_list)
 
 
 
